3d8a9755d53011c19d45e0e975f465d66ee1d774
Emmanuel Adekola <54865887+dataclergy@users.noreply.github.com>
2021-04-27 16:48:38 +0100

    Update CommCare Feature Enginering.py

--- CommCare Feature Enginering.py
+++ CommCare Feature Enginering.py
@@ -231,3 +231,20 @@ for result_caseid in list(set(new_data['Results caseid'])):
     #Export to Excel file
     output_data = pd.concat([output_data,x])
     output_data.to_excel('Output.xlsx', index = False)
+        
+    
+#upload to sharepoint online   
+#load libraries needed for sharepoint upload
+from shareplum import Site
+from shareplum import Office365
+from shareplum.site import Version
+
+import pandas as pd
+#setup access to sharepoint online
+authcookie = Office365('https://urlname.sharepoint.com', username='email', password='password').GetCookies()
+site = Site('https://urlname.sharepoint.com/sites/', version=Version.v2016, authcookie=authcookie)
+folder = site.Folder('Shared Documents/Folder/')
+
+with open('Output.xlsx', mode='rb') as file:
+        fileContent = file.read()
+folder.upload_file(fileContent, "Output.xlsx")
